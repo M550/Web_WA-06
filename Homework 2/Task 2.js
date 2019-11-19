85319b848460505c5afed00bc9e3760fd1bd0661
M550 <57444434+M550@users.noreply.github.com>
2019-11-19 11:41:01 +0300

    new pics, HW2

---
+++ Homework 2/Task 2.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+    var name = "";
+    var username = prompt("Please enter username");
+    if (/\d/.test(username) == true) {                //проверка на наличие цифры
+       
+        name = username.split("").reverse("").join("");
+
+    } else {
+        for (var i = 0; i <= username.length - 1; i++) {
+            if (i % 2) {     // если делится на 2 без остатка => чётная
+                name += username[i].toLowerCase();
+            } else {
+                name += username[i].toUpperCase();
+            }
+
+        }   
+    }
+    document.getElementById("username").innerHTML = name;
+}
